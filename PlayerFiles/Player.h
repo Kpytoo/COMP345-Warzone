98bf0532e2cfeb3a2ced631fdaf35aa09f53c420
nokir03 <117036571+nokir03@users.noreply.github.com>
2024-09-23 20:23:14 -0400

    Update Player.h

--- PlayerFiles/Player.h
+++ PlayerFiles/Player.h
@@ -4,18 +4,43 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
 #include "MapFiles/Map.h"
 
 class Player
 {
 
-public:
+private:
+    std::string playerName;
     std::vector<Territory *> OwnedTerritories;
-//    std::vector<Hand *> PlayerHand; // Need to implement deck and hand
+    Hand *playerHand;       // Need to implement deck and hand
+    OrdersList *ordersList; // orderList object need to be implemented
+    int numArmies;          // idk if necessary?
+
+public:
+    Player();
+    Player(std::string playerName);
+    Player(const Player &other);
+    Player &operator=(const Player &other);
+    friend std::ostream &operator<<(std::ostream &os, const Player &obj);
+    ~Player();
+
+    // Getters
+    std::string getPlayerName() const;
+    std::vector<Territory *> getOwnedTerritories() const;
+    Hand *getPlayerHand() const;
+    OrdersList *getOrdersList() const;
+    int getNumArmies() const;
+
+    // Setters
+    void setPlayerName(const std::string &name);
+    void setOwnedTerritories(const std::vector<Territory *> &territories);
+    void setPlayerHand(Hand *hand);
+    void setOrdersList(OrdersList *ordersList);
+    void setNumArmies(int numArmies);
 
     std::vector<Territory *> toDefend();
     std::vector<Territory *> toAttack();
+    void issueOrder();
 };
 
 #endif
