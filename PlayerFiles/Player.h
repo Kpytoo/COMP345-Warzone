092ca98688d7af3e28ed8238660db7a37d49dff0
nokir03 <117036571+nokir03@users.noreply.github.com>
2024-10-04 15:06:00 -0400

    Update Player.h

--- PlayerFiles/Player.h
+++ PlayerFiles/Player.h
@@ -23,7 +23,7 @@ private:
 public:
     // Constructors, assignment operator, and destructor
     Player();
-    Player(std::string playerName);
+    Player(std::string playerName, const std::vector<Territory *> &ownedTerritories);
     Player(const Player &other);
     Player &operator=(const Player &other);
     friend std::ostream &operator<<(std::ostream &os, const Player &obj);
