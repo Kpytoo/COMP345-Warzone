6660c062c0334c9a8a4bdce47e967246dbf809d9
nokir03 <117036571+nokir03@users.noreply.github.com>
2024-10-03 23:32:20 -0400

    Update Player.h

--- PlayerFiles/Player.h
+++ PlayerFiles/Player.h
@@ -5,18 +5,21 @@
 #include <vector>
 #include <string>
 #include "MapFiles/Map.h"
+#include "CardsFiles/Cards.h"
+#include "/OrdersList.h"       // Placeholder for OrdersList, to be included when implemented
+
 
 class Player
 {
-
 private:
-    std::string playerName;
-    std::vector<Territory *> OwnedTerritories;
-    Hand *playerHand;       // Need to implement deck and hand
-    OrdersList *ordersList; // orderList object need to be implemented
-    int numArmies;          // idk if necessary?
+    std::string playerName;               // Player's name
+    std::vector<Territory *> OwnedTerritories; // List of territories owned by the player
+    Hand *playerHand;                     // Pointer to the player's hand of cards
+    OrdersList *ordersList;               // Pointer to the player's orders list
+    int numArmies;                        // Number of armies player controls
 
 public:
+    // Constructors, assignment operator, and destructor
     Player();
     Player(std::string playerName);
     Player(const Player &other);
@@ -24,23 +27,24 @@ public:
     friend std::ostream &operator<<(std::ostream &os, const Player &obj);
     ~Player();
 
-    // Getters
+    // Getters for accessing private member variables
     std::string getPlayerName() const;
     std::vector<Territory *> getOwnedTerritories() const;
     Hand *getPlayerHand() const;
     OrdersList *getOrdersList() const;
     int getNumArmies() const;
 
-    // Setters
+    // Setters for modifying private member variables
     void setPlayerName(const std::string &name);
     void setOwnedTerritories(const std::vector<Territory *> &territories);
     void setPlayerHand(Hand *hand);
     void setOrdersList(OrdersList *ordersList);
     void setNumArmies(int numArmies);
 
-    std::vector<Territory *> toDefend();
-    std::vector<Territory *> toAttack();
-    void issueOrder();
+    // Core gameplay methods
+    std::vector<Territory *> toDefend(); // Returns territories the player should defend
+    std::vector<Territory *> toAttack(); // Returns territories the player may attack
+    void issueOrder();                   // Issues an order for the player
 };
 
 #endif
