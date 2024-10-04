b10fbe702f7f3f03e8321a6ad77153fc9f5dd897
nokir03 <117036571+nokir03@users.noreply.github.com>
2024-10-04 12:07:16 -0400

    Update Player.h

--- PlayerFiles/Player.h
+++ PlayerFiles/Player.h
@@ -14,6 +14,8 @@ class Player
 private:
     std::string playerName;               // Player's name
     std::vector<Territory *> OwnedTerritories; // List of territories owned by the player
+    std::vector<Territory *> *toDefendTerritories; // Pointer list to OwnedTerritories elements to defend
+    std::vector<Territory *> toAttackTerritories; // List of territories the player wants to attack
     Hand *playerHand;                     // Pointer to the player's hand of cards
     OrdersList *ordersList;               // Pointer to the player's orders list
     int numArmies;                        // Number of armies player controls
@@ -30,6 +32,8 @@ public:
     // Getters for accessing private member variables
     std::string getPlayerName() const;
     std::vector<Territory *> getOwnedTerritories() const;
+    std::vector<Territory *> getToDefendTerritories() const;
+    std::vector<Territory *> getToAttackTerritories() const;
     Hand *getPlayerHand() const;
     OrdersList *getOrdersList() const;
     int getNumArmies() const;
@@ -37,6 +41,8 @@ public:
     // Setters for modifying private member variables
     void setPlayerName(const std::string &name);
     void setOwnedTerritories(const std::vector<Territory *> &territories);
+    void setToAttackTerritories(const std::vector<Territory *> &territories);
+    void setToDefendTerritories(std::vector<Territory *> *territories);
     void setPlayerHand(Hand *hand);
     void setOrdersList(OrdersList *ordersList);
     void setNumArmies(int numArmies);
