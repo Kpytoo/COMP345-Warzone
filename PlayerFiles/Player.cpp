f75db79558bded89134b0f55b4a17d2d0f1f28d5
nokir03 <117036571+nokir03@users.noreply.github.com>
2024-10-04 12:31:08 -0400

    Update Player.cpp

--- PlayerFiles/Player.cpp
+++ PlayerFiles/Player.cpp
@@ -24,6 +24,7 @@ Player::~Player()
 {
     delete playerHand;
     delete ordersList;
+    delete toDefendTerritories;
 }
 
 // Copy constructor: creates a deep copy of another Player object
@@ -40,6 +41,8 @@ Player::Player(const Player &other)
 
     // Shallow copy of owned territories (assuming pointer structure is shared)
     OwnedTerritories = other.OwnedTerritories;
+    toAttackTerritories = other.toAttackTerritories
+    toDefendTerritories = new std::vector<Territory *>(*other.toDefendTerritories);
 }
 
 // Assignment operator: assigns deep copies of resources to the current Player
@@ -55,16 +58,19 @@ Player &Player::operator=(const Player &other)
     // Clean up current resources
     delete playerHand;
     delete ordersList;
-
+    delete toDefendTerritories
     // Allocate new copies for playerHand and ordersList
-//    playerHand = new Hand(*other.playerHand);
-//    ordersList = new OrdersList(*other.ordersList);
+    playerHand = new Hand(*other.playerHand);
+    //ordersList = new OrdersList(*other.ordersList);
     playerHand = new Hand();
     ordersList = new OrdersList();
 
     // Shallow copy of owned territories
     OwnedTerritories = other.OwnedTerritories;
+    toAttackTerritories = other.toAttackTerritories
 
+    //New territories to defend list
+    toDefendTerritories = new std::vector<Territory *>(*other.toDefendTerritories);
     return *this;
 }
 
@@ -81,6 +87,20 @@ std::ostream &operator<<(std::ostream &os, const Player &obj)
     }
     os << std::endl;
 
+     os << "To Defend Territories: ";
+    for (auto &territory : *(obj.toDefendTerritories))
+    {
+        os << territory->name << " "; // Outputs each territory's name
+    }
+    os << std::endl;
+
+    os << "To Attack Territories: ";
+    for (auto &territory : obj.toAttackTerritories)
+    {
+        os << territory->name << " "; // Outputs each territory's name
+    }
+    os << std::endl;
+    
     // Outputs player's hand and orders list (assuming operator<< is defined for Hand and OrdersList)
 //    os << "Player Hand: " << *(obj.playerHand) << std::endl; // TODO: uncomment this and the ones in the copy functions
 //    os << "Orders List: " << *(obj.ordersList) << std::endl;
@@ -91,6 +111,8 @@ std::ostream &operator<<(std::ostream &os, const Player &obj)
 // Getter definitions: retrieve player details
 std::string Player::getPlayerName() const { return playerName; }
 std::vector<Territory *> Player::getOwnedTerritories() const { return OwnedTerritories; }
+std::vector<Territory *> Player::getToDefendTerritories() const { return *toDefendTerritories; }
+std::vector<Territory *> Player::getToAttackTerritories() const { return toAttackTerritories; }
 Hand *Player::getPlayerHand() const { return playerHand; }
 OrdersList *Player::getOrdersList() const { return ordersList; }
 int Player::getNumArmies() const { return numArmies; }
@@ -98,20 +120,22 @@ int Player::getNumArmies() const { return numArmies; }
 // Setter definitions: modify player details
 void Player::setPlayerName(const std::string &name) { playerName = name; }
 void Player::setOwnedTerritories(const std::vector<Territory *> &territories) { OwnedTerritories = territories; }
+void Player::setToDefendTerritories(std::vector<Territory *> *territories) { toDefendTerritories = territories; }
+void Player::setToAttackTerritories(const std::vector<Territory *> &territories) { OwnedTerritories = territories; }
 void Player::setPlayerHand(Hand *hand) { playerHand = hand; }
 void Player::setOrdersList(OrdersList *ordersList) { this->ordersList = ordersList; }
 void Player::setNumArmies(int numArmies) { this->numArmies = numArmies; }
 
-// toDefend: returns a list of territories to defend (placeholder behavior returning owned territories)
+// toDefend: returns a list of territories to defend
 std::vector<Territory *> Player::toDefend()
 {
-    return OwnedTerritories;
+    return *toDefendTerritories;
 }
 
-// toAttack: returns a list of territories to attack (placeholder behavior returning owned territories)
+// toAttack: returns a list of territories to attack
 std::vector<Territory *> Player::toAttack()
 {
-    return OwnedTerritories;
+    return toAttackTerritories;
 }
 
 // issueOrder: creates a new order and adds it to the player's orders list
